27abf490fba56954a958cd40e898b3f605f3b6f6
satoshikizuki <sktfhj@icloud.com>
2019-07-16 20:47:03 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:satoshikizuki/chat-space.git"
+set :repo_url, "git@github.com:satoshikizuki/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
